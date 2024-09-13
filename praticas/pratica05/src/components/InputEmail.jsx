02d8f7551d67cb751cf090082170de15ef7bb629
lucius <ludluxx@gmail.com>
2024-09-12 21:17:52 -0300

    fizemos a pratica05, deu tudo certo graças a Deus

---
+++ praticas/pratica05/src/components/InputEmail.jsx
@@ -0,0 +1,15 @@
+import "./Input.css";
+
+function InputEmail() {
+    return (
+     <>
+       <label htmlFor="email">E-mail</label>
+       <input type="email" id="email" name="email" required/>
+     </>
+    );
+  }
+  
+ 
+  
+  export default InputEmail;
+  
